3cc253ab4f2f69e842ef5fe7efe39c1ed33aa013
Ivan Romaniv <i.romaniv@relevant.software>
2025-05-14 15:01:40 +0700

    add chi

--- cmd/chatserver/main.go
+++ cmd/chatserver/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/i474232898/chatserver/internal/app/server"
+	"github.com/i474232898/chatserver/pkg/db"
+	"github.com/joho/godotenv"
+)
 
 var p = fmt.Println
 
 func main() {
-	p("Hello chatserver<")
+	err := godotenv.Load()
+
+	if err != nil {
+		panic("incomplete environment vars")
+	}
+
+	db.Connect()
+	server.Start(os.Getenv("PORT"))
 }
