bed5be273eeb0fbd4bdac1dab736326a82ad066e
Ivan Romaniv <i474232898@gmail.com>
2025-05-19 20:19:00 +0700

    move db conn to repository

--- cmd/chatserver/main.go
+++ cmd/chatserver/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/i474232898/chatserver/configs"
+	"github.com/i474232898/chatserver/internal/app/repositories"
 	"github.com/i474232898/chatserver/internal/app/server"
-	"github.com/i474232898/chatserver/pkg/db"
 )
 
 func main() {
 	cnf := configs.New()
-	err := db.Connect(cnf)
+	err := repositories.Connect(cnf)
 	if err != nil {
 		panic("Can't connect to db")
 	}
