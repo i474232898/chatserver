a5e94a9052191e2f33b216e15c20d4025470b9dd
Ivan Romaniv <i474232898@gmail.com>
2025-05-19 20:11:02 +0700

    refactor structure

---
+++ internal/app/repositories/user.go
@@ -0,0 +1,31 @@
+package repositories
+
+import (
+	"context"
+
+	// "github.com/i474232898/chatserver/internal/app/dto"
+	"github.com/i474232898/chatserver/internal/app/repositories/models"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user *models.User) (*models.User, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
+}
+
+func (r *userRepository) Create(ctx context.Context, user *models.User) (*models.User, error) {
+	result := r.db.Create(user)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return user, nil
+}
